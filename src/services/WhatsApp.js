9494250b504918def71590c48b4b69d7e71f1361
Khadim <khadimfall0602@gmail.com>
2021-04-26 22:44:16 +0200

    added auth

--- src/services/WhatsApp.js
+++ src/services/WhatsApp.js
@@ -2,6 +2,10 @@ const axios = require("axios").default;
 
 const client = axios.create({
     baseURL: process.env.WHATSAPP_API_URL,
+    auth: {
+        username: process.env.WHATSAPP_API_USER,
+        password: process.env.WHATSAPP_API_PASS
+    }
 });
 
 module.exports = {

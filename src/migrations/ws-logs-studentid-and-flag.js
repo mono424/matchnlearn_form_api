b2f2f73eaea98b1c4f09892afe6d768a3179fa53
Khadim <khadimfall0602@gmail.com>
2021-05-02 11:25:04 +0200

    added ws-flag migration

---
+++ src/migrations/ws-logs-studentid-and-flag.js
@@ -0,0 +1,27 @@
+/**
+ * @param {MongoClient} db 
+ */
+module.exports = async function (db) {
+    const logs = await db.collection('whatsapp-logs').find().toArray();
+    for (const log of logs) {
+        if (log.meta.number) {
+            const student = await db.collection('students').findOne({ phoneNumber: log.meta.number });
+            
+            if (student) {
+                await db.collection('whatsapp-logs').updateOne(
+                    { _id: log._id },
+                    { $set: { meta: { ...log.meta, studentId: student._id.toString() } } }
+                );
+                await db.collection('students').updateOne(
+                    { _id: student._id },
+                    { $set: { validWhatsAppNumber: log.status === "success" } }
+                );
+            } else {
+                await db.collection('whatsapp-logs').deleteOne(
+                    { _id: log._id },
+                );
+            }
+            
+        }
+    }
+};

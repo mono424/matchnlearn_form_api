b7d21ab255ea479b7c2b3b36634480d88c65cceb
Khadim <khadimfall0602@gmail.com>
2021-05-03 18:44:47 +0200

    updated format of csv

--- src/api/export.js
+++ src/api/export.js
@@ -10,8 +10,34 @@ module.exports = {
                 auth: 'simple'
             },
             handler: async (request, h) => {
-                const students = await db.getClient().db().collection("students").find().sort({ "createdAt": -1 });
-                const csv = json2csv.parse(await students.toArray());
+                const students = await db.getClient().db().collection("students").aggregate([
+                    { 
+                        $lookup: {
+                        from: 'courses',
+                        localField: 'courses',
+                        foreignField: '_id',
+                        as: '_courses'
+                      }
+                    }
+                ])
+                .sort({ "createdAt": -1 });
+
+                const formatted = (await students.toArray()).map(record => {
+                    let nrecord = {
+                        ...record,
+                        courses: record._courses.map(course => course.names[0]).join(", "),
+                        preferences: record.preferences.join(", "),
+                        degree: record.studyStatus.degree,
+                        year: record.studyStatus.year,
+                        _courses: undefined,
+                        studyStatus: undefined,
+                    }
+                    delete nrecord._courses;
+                    delete nrecord.studyStatus;
+                    return nrecord;
+                });
+
+                const csv = json2csv.parse(formatted);
                 return h.response(csv)
                     .header('Content-Type', "text/csv")
                     .header('Content-Disposition', 'attachment; filename= ' + "students.csv");

19e0c3fe1a25f0f600215cf4277df50bccc614d7
Khadim <khadimfall0602@gmail.com>
2021-06-20 15:37:17 +0200

    added new api for new form

--- src/api/student.js
+++ src/api/student.js
@@ -1,19 +1,20 @@
 const Joi = require('@hapi/joi');
+const Boom = require('@hapi/boom');
 const { ObjectId } = require('mongodb');
 const db = require('../db');
 const WhatsAppService = require('../services/WhatsApp');
 
 const confirmationMessage = ({ name }) => `**MatchNLearn Confirmation** Hey ${name}, thank you for using MatchNLearn to find a study-group.`;
 
-const FACULTIES = ["in", "wi", "mw", "ei", "other"];
-const DEGREES = ["ba", "ma"];
-const YEARS = ["1", "2", "3", "4+"];
-const EXPERIENCES = ["easy", "alright", "difficult", "struggeling", "first_semester"];
-const ATTITUDES = ["learning_lover", "score_chaser", "efficient", "pass_prayer"];
-const GROUPSIZES = ["two", "three", "three_or_more", "dont_care"];
-const LANGUAGES = ["german", "english", "both"];
-const PROGRAMSCOPES = ["all", "only_my_program"];
-const PREFERENCES = ["fun", "productivity", "munich"];
+const GRADES = [1, 2, 3, 4];
+const PREPARATION = ["A", "B", "C", "D", "E"];
+
+const validKeysOfCourseRelatedObject = (obj, courses) => {
+    for (const key of Object.keys(obj)) {
+        if (!courses.includes(key)) return false;
+    }
+    return true;
+}
 
 module.exports = {
     routes: () => [
@@ -29,27 +30,30 @@ module.exports = {
                     payload: Joi.object({ 
                         name: Joi.string().required(),
                         phoneNumber: Joi.string().regex(/^\+[0-9]+/).required(),
-                        faculty: Joi.string().valid(...FACULTIES).required(),
                         courses: Joi.array().min(1).max(4).items(Joi.string()).required(),
-                        studyStatus: Joi.object({
-                            degree: Joi.string().valid(...DEGREES).required(),
-                            year: Joi.string().valid(...YEARS).required(),
-                        }),
-                        prevSemesterExperience: Joi.string().valid(...EXPERIENCES).required(),
-                        attitude: Joi.string().valid(...ATTITUDES).required(),
-                        comment: Joi.string(),
-                        groupSize: Joi.string().valid(...GROUPSIZES).required(),
-                        language: Joi.string().valid(...LANGUAGES).required(),
-                        programScope: Joi.string().valid(...PROGRAMSCOPES).required(),
-                        preferences: Joi.array().items(Joi.string().valid(...PREFERENCES)).required(),
+                        grades: Joi.object(),
+                        preparation: Joi.object(),
                     })
                 }
             },
             handler: async (request, h) => {
                 const { payload } = request;
+                
+                // Validate grades and preparation
+                if (
+                    !validKeysOfCourseRelatedObject(payload.grades, payload.courses) ||
+                    Object.values(payload.grades).filter(g => !GRADES.includes(g)).length > 0
+                ) return Boom.badRequest("Invalid format of key 'grades'.");
+                if (
+                    !validKeysOfCourseRelatedObject(payload.preparation, payload.courses) ||
+                    Object.values(payload.preparation).filter(p => !PREPARATION.includes(p)).length > 0
+                ) return Boom.badRequest("Invalid format of key 'grades'.");
+
+                // Add additional information and create record and send whatsapp message
                 payload.courses = payload.courses.map(c => ObjectId(c));
                 payload.phoneNumber = payload.phoneNumber.replace(/\s/g, "");
                 payload.createdAt = new Date();
+                payload.version = 2;
                 const result = await db.getClient().db().collection("students").insertOne(payload);
                 WhatsAppService.sendMessage(result.insertedId, confirmationMessage(payload));
                 return {
